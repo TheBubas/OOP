e71722392563860818329d13030960fe64016510
z907 <batshat15558@gmail.com>
2023-09-23 09:39:51 +0300

    asjsa

--- Cons/Program.cs
+++ Cons/Program.cs
@@ -1,13 +1,1 @@
-﻿using System;
-
-namespace Cons
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello dungeon master!");
-            Console.WriteLine("Goodbye");
-        }
-    }
-}
+﻿
